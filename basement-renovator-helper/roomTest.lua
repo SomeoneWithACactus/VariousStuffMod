335ddfad112844008bef2a6fc077a552faf3d6f8
SomeoneWithACactus <alej36711@gmail.com>
2024-11-29 17:42:06 -0400

    Add files via upload

---
+++ basement-renovator-helper/roomTest.lua
@@ -0,0 +1,19 @@
+return {
+    TestType = "InstaPreview",
+    Character = nil, -- only used in Repentance
+    Commands = {  },
+    Stage = 1,
+    StageType = 0,
+    StageName = "Basement",
+    IsModStage = false,
+    RoomFile = "C:\\Users\\ALEJANDRO\\Documents\\GAMES\\The Bindig of Isaac Repentance v.1.7.9b\\mods\\VariousStuffMod\\content\\rooms\\01.basement.xml",
+    Rooms = {
+    {
+        Name = "New Room",
+        Type = 1,
+        Variant = 0,
+        Subtype = 0,
+        Shape = 1
+    }
+    }
+}
---
+++ content/rooms/basement-renovator-helper/roomTest.lua
@@ -0,0 +1,19 @@
+return {
+    TestType = "InstaPreview",
+    Character = nil, -- only used in Repentance
+    Commands = {  },
+    Stage = 3,
+    StageType = 0,
+    StageName = "Caves",
+    IsModStage = false,
+    RoomFile = "C:\\Users\\ALEJANDRO\\Documents\\GAMES\\The Bindig of Isaac Repentance v.1.7.9b\\mods\\VariousStuffMod\\content\\rooms\\04.caves.xml",
+    Rooms = {
+    {
+        Name = "Butt Explosion!",
+        Type = 1,
+        Variant = 0,
+        Subtype = 0,
+        Shape = 1
+    }
+    }
+}
